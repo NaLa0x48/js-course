4f1fde8ac25459a2c5c14def9714c61d949af292
NaLa0x48 <86408193+NaLa0x48@users.noreply.github.com>
2022-06-01 13:21:58 +0630

    Add files via upload

---
+++ chapter 2/sandbox.js
@@ -0,0 +1,13 @@
+let age = 25
+let year = 2014
+//this is a comment
+
+console.log(age, year)
+age = 30
+console.log(age)
+
+/* const points = 100
+console.log(points)
+
+var score = 75
+console.log(score) */
