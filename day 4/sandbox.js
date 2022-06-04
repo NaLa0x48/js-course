3b7f64263763b985722a4e8b5f213ac0e55dc6a3
NaLa0x48 <86408193+NaLa0x48@users.noreply.github.com>
2022-06-04 14:04:01 +0630

    Add files via upload
    loops

---
+++ day 4/sandbox.js
@@ -0,0 +1,35 @@
+//for loops
+// for (let i = 0; i < 5; i++) {
+//   console.log('in loop:', i)
+// }
+
+// console.log('loop finished')
+
+// const names = ['nae', 'nay lin', 'mario']
+
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i])
+//   let html = `<div>${names[i]}</div>`
+//   console.log(html)
+// }
+
+//while loops
+
+//let i = 0
+const names = ['nae', 'nay lin', 'mario']
+// while (i < 5) {
+//   console.log('in loop:', i)
+//   i++
+// }
+
+// while (i < names.length) {
+//   console.log(names[i])
+//   i++
+// }
+
+//do while
+let i = 5
+do {
+  console.log('val i is: ', i)
+  i++
+} while (i < 5)
