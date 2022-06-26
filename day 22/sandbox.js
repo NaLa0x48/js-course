975dd54f285b612106e1cca2bcd5b81100ec9136
NaLa0x48 <86408193+NaLa0x48@users.noreply.github.com>
2022-06-26 12:14:24 +0630

    Add files via upload

--- day 22/sandbox.js
+++ day 22/sandbox.js
@@ -1,80 +1,32 @@
-// console.log(1)
-// console.log(2)
-// setTimeout(() => {
-//   console.log('call back function fire')
-// }, 2000)
-
-// console.log(3)
-// console.log(4)
-const getPosts = (resource, callback) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest()
-    request.addEventListener('readystatechange', () => {
-      //console.log(request, request.responseText)
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText)
-        resolve(data)
-      } else if (request.readyState === 4) {
-        reject('error getting resource')
-      }
-    })
-    request.open('GET', resource)
-    request.send()
-  })
-}
-// console.log(1)
-// console.log(2)
-// getPosts('todos/luigi.json', (err, data) => {
-//   console.log(data)
-//   getPosts('todos/mario.json', (err, data) => {
-//     console.log(data)
-//     getPosts('todos/shuan.json', (err, data) => {
-//       console.log(data)
-//     })
+//fetch api
+//no reject in fetch if there is no network error
+// fetch('todos/luigi.json')
+//   .then((response) => {
+//     console.log('reslove:', response)
+//     return response.json()
 //   })
-// })
+//   .then((data) => {
+//     console.log(data)
+//   })
+//   .catch((err) => {
+//     console.log('reject:', err)
+//   })
 
-// console.log(3)
-// console.log(4)
+//async await
 
-getPosts('todos/luigi.json')
-  .then((data) => {
-    console.log('promised result :', data)
-    return getPosts('todos/mario.json')
-  })
-  .then((data) => {
-    console.log('promised 2 result : ', data)
-    return getPosts('todos/shuan.json')
-  })
-  .then((data) => {
-    console.log('promised 3 result: ', data)
-  })
-  .catch((err) => {
-    console.log('promised rejected:', err)
-  })
-//promise example
-
-const getSomething = () => {
-  return new Promise((resolve, reject) => {
-    //fetch something
-    //resolve('some data')
-    reject('some error')
-  })
+const getPosts = async () => {
+  const response = await fetch('todos/luigis.json')
+  if (response.status != 200) {
+    throw new Error('cannot fetch the data')
+  }
+  const data = await response.json()
+  return data
 }
 
-// getSomething().then(
-//   (data) => {
-//     console.log(data)
-//   },
-//   (err) => {
-//     console.log(err)
-//   }
-// )
-
-getSomething()
+getPosts()
   .then((data) => {
-    console.log(data)
+    console.log('resolve:', data)
   })
   .catch((err) => {
-    console.log(err)
+    console.log('reject:', err.message)
   })
