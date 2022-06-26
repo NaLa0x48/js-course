d3617bbe99503fe839584c0ebde27d5b1e99a709
NaLa0x48 <86408193+NaLa0x48@users.noreply.github.com>
2022-06-26 11:47:32 +0630

    Add files via upload
    callback hell

--- day 21/sandbox.js
+++ day 21/sandbox.js
@@ -7,30 +7,66 @@
 // console.log(3)
 // console.log(4)
 const getPosts = (resource, callback) => {
-  const request = new XMLHttpRequest()
-  request.addEventListener('readystatechange', () => {
-    //console.log(request, request.responseText)
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText)
-      callback(undefined, data)
-    } else if (request.readyState === 4) {
-      callback('could not fetch data', undefined)
-    }
-  })
-  request.open('GET', resource)
-  request.send()
-}
-console.log(1)
-console.log(2)
-getPosts('todos/luigi.json', (err, data) => {
-  console.log(data)
-  getPosts('todos/mario.json', (err, data) => {
-    console.log(data)
-    getPosts('todos/shuan.json', (err, data) => {
-      console.log(data)
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest()
+    request.addEventListener('readystatechange', () => {
+      //console.log(request, request.responseText)
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText)
+        resolve(data)
+      } else if (request.readyState === 4) {
+        reject('error getting resource')
+      }
     })
+    request.open('GET', resource)
+    request.send()
   })
-})
+}
+// console.log(1)
+// console.log(2)
+// getPosts('todos/luigi.json', (err, data) => {
+//   console.log(data)
+//   getPosts('todos/mario.json', (err, data) => {
+//     console.log(data)
+//     getPosts('todos/shuan.json', (err, data) => {
+//       console.log(data)
+//     })
+//   })
+// })
 
-console.log(3)
-console.log(4)
+// console.log(3)
+// console.log(4)
+
+getPosts('todos/luigi.json')
+  .then((data) => {
+    console.log('promised result :', data)
+  })
+  .catch((err) => {
+    console.log('promised rejected:', err)
+  })
+//promise example
+
+const getSomething = () => {
+  return new Promise((resolve, reject) => {
+    //fetch something
+    //resolve('some data')
+    reject('some error')
+  })
+}
+
+// getSomething().then(
+//   (data) => {
+//     console.log(data)
+//   },
+//   (err) => {
+//     console.log(err)
+//   }
+// )
+
+getSomething()
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
